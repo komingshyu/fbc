komingshyu/fbc

commit a4dbfff4e51858379b13e253b24fa943b9b745ff

-pic option when linking now passes -pie to ld, to create a position independent executable

src/compiler/fbc.bas
@@ -746,6 +746,10 @@ private function hLinkFiles( ) as integer
 		ldcline += " -Bstatic"
 	end if
 
+	if( fbGetOption( FB_COMPOPT_PIC ) ) then
+		ldcline += " -pie"
+	end if
+
 	if( len( fbc.mapfile ) > 0) then
 		ldcline += " -Map " + fbc.mapfile
 	end if
@@ -2212,7 +2216,7 @@ end sub
 '' Whether a target needs shared libraries to be built with PIC.
 '' (Note: Android 5.0+ also need executables to be built with PIC (gcc -pie argument),
 '' but Android 4.0- don't support PIE executables. So it's up to the user to decide
-'' and pass argument to gcc.)
+'' and pass that argument to gcc.)
 private function hTargetNeedsPIC( ) as integer
 	function = FALSE
 	if( fbGetCpuFamily( ) <> FB_CPUFAMILY_X86 ) then

commit 0df588e180cb21e8c8f931c96f6f550a10d19bbd

Fix compiling dynamic libs (which I broke in a4dbfff4)

src/compiler/fbc.bas
@@ -938,7 +938,11 @@ private function hLinkFiles( ) as integer
 	end if
 
 	if( fbGetOption( FB_COMPOPT_PIC ) ) then
-		ldcline += " -pie"
+		if( fbGetOption( FB_COMPOPT_OUTTYPE ) = FB_OUTTYPE_EXECUTABLE ) then
+			ldcline += " -pie"
+		else
+			'' Dynamic library: no flag needed
+		end if
 	end if
 
 	if( len( fbc.mapfile ) > 0) then